92e0365ebea1157a3551aaf7bf22b606622bf84e
farrelljade <farrellmikel@yahoo.com>
2024-01-27 23:09:43 +0000

    Add user profile page. Add ability to follow/view users profile pages.

--- social/m/views.py
+++ social/m/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Profile
 
 def home(request):
@@ -6,6 +7,20 @@ def home(request):
 
 
 def profile_list(request):
-    # Retrieve a list of profiles excluding the users profile
-    profiles = Profile.objects.exclude(user=request.user)
-    return render(request, 'm/profile_list.html', {'profiles': profiles})
+    if request.user.is_authenticated:
+        """Retrieve a list of profiles excluding the users profile."""
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'm/profile_list.html', {'profiles': profiles})
+    else:
+        messages.error(request, "You must be logged in to view this page...")
+        return redirect('m:home')
+    
+
+def profile(request, pk):
+    """User profile page."""
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, 'm/profile.html', {'profile': profile})
+    else:
+        messages.error(request, "You must be logged in to view this page...")
+        return redirect('m:home')
